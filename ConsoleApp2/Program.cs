fffcec05b32b36401176981e687429226079eee2
Rawan Eldamanhory <rawaneldamanhory@gmail.com>
2026-01-16 17:30:44 +0200

    Initial commit: AVL Tree implementation in C#

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class AVL
+    {
+        class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+        Node root;
+       
+        //insert operation
+        public void Add(int data)
+        {
+            Node newItem = new Node(data);
+            if (root == null)
+            {
+                root = newItem;
+            }
+            else
+            {
+                root = RecursiveInsert(root, newItem);
+            }
+        }
+
+        private Node RecursiveInsert(Node current, Node n)
+        {
+            if (current == null)
+            {
+                current = n;
+                return current;
+            }
+            else if (n.data < current.data)
+            {
+                current.left = RecursiveInsert(current.left, n);
+                current = balance_tree(current);
+            }
+            else if (n.data > current.data)
+            {
+                current.right = RecursiveInsert(current.right, n);
+                current = balance_tree(current);
+            }
+            return current;
+        }
+
+        private Node balance_tree(Node current)
+        {
+            int b_factor = balance_factor(current);
+            if (b_factor > 1)
+            {
+                if (balance_factor(current.left) > 0)
+                {
+                    current = RotateLL(current);
+                }
+                else
+                {
+                    current = RotateLR(current);
+                }
+            }
+            else if (b_factor < -1)
+            {
+                if (balance_factor(current.right) > 0)
+                {
+                    current = RotateRL(current);
+                }
+                else
+                {
+                    current = RotateRR(current);
+                }
+            }
+            return current;
+        }
+        
+        //delete operation
+        public void Delete(int target)
+        {
+            root = Delete(root, target);
+        }
+
+        private Node Delete(Node current, int target)
+        {
+            Node parent;
+            if (current == null)            //tree is empty
+            {
+                return null; 
+            }
+
+            else
+            {                                    
+                if (target < current.data)              //left subtree
+                {
+                    current.left = Delete(current.left, target);
+                    if (balance_factor(current) == -2)
+                    {
+                        if (balance_factor(current.right) <= 0)
+                        {
+                            current = RotateRR(current);
+                        }
+                        else
+                        {
+                            current = RotateRL(current);
+                        }
+                    }
+                }
+
+                else if (target > current.data)          //right subtree
+                {
+                    current.right = Delete(current.right, target);
+                    if (balance_factor(current) == 2)
+                    {
+                        if (balance_factor(current.left) >= 0)
+                        {
+                            current = RotateLL(current);
+                        }
+                        else
+                        {
+                            current = RotateLR(current);
+                        }
+                    }
+                }
+
+                else                                 //if target is found
+                {
+                    if (current.right != null)
+                    {                                 //delete its inorder successor
+                        parent = current.right;
+                        while (parent.left != null)
+                        {
+                            parent = parent.left;
+                        }
+                        current.data = parent.data;
+                        current.right = Delete(current.right, parent.data);
+                        if (balance_factor(current) == 2)                        //rebalancing
+                        {
+                            if (balance_factor(current.left) >= 0)
+                            {
+                                current = RotateLL(current);
+                            }
+                            else 
+                            { 
+                                current = RotateLR(current);
+                            }
+                        }
+                    }
+                    else
+                    {                              //if current.left != null
+                        return current.left;
+                    }
+                }
+            }
+            return current;
+        }
+
+        //search operation
+        public bool Find(int key)
+        {
+            return Find(key, root) != null;
+        }
+
+        private Node Find(int target, Node current)
+        {
+            if (current == null)
+                return null;
+
+            if (target == current.data)
+                return current;
+            else if (target < current.data)
+                return Find(target, current.left);
+            else
+                return Find(target, current.right);
+        }
+
+
+        //display tree 
+        public void DisplayTree()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            InOrderDisplayTree(root);
+            Console.WriteLine();
+        }
+
+        //inorder display tree
+        private void InOrderDisplayTree(Node current)
+        {
+            if (current != null)
+            {
+                InOrderDisplayTree(current.left);
+                Console.Write("({0}) ", current.data);
+                InOrderDisplayTree(current.right);
+            }
+        }
+
+        //get maximum node
+        private int max(int l, int r)
+        {
+            return l > r ? l : r;
+        }
+
+        //get the height
+        private int getHeight(Node current)
+        {
+            int height = 0;
+            if (current != null)
+            {
+                int l = getHeight(current.left);
+                int r = getHeight(current.right);
+                int m = max(l, r);
+                height = m + 1;
+            }
+            return height;
+        }
+
+        //get balance factor
+        private int balance_factor(Node current)
+        {
+            int l = getHeight(current.left);
+            int r = getHeight(current.right);
+            int b_factor = l - r;
+            return b_factor;
+        }
+
+        //RR Rotation Operation
+        private Node RotateRR(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = pivot.left;
+            pivot.left = parent;
+            return pivot;
+        }
+
+        //LL Rotation Operation
+        private Node RotateLL(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = pivot.right;
+            pivot.right = parent;
+            return pivot;
+        }
+
+        //LR Rotation Operation
+        private Node RotateLR(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = RotateRR(pivot);
+            return RotateLL(parent);
+        }
+
+        //RL Rotation Operation
+        private Node RotateRL(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = RotateLL(pivot);
+            return RotateRR(parent);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVL tree = new AVL();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+            tree.DisplayTree();
+            tree.Delete(7);
+            tree.DisplayTree();
+            Console.WriteLine(tree.Find(3) ? "3 was found!" : "Not found!");
+
+            Console.ReadKey();
+        }
+    }
+}
